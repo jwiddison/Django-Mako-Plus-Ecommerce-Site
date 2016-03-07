8d8090653c0cbbc487df55628aa3e6426a715027
Jordan Widdison <jordan.widdison@gmail.com>
2016-03-07 09:26:48 -0700

    Testing changes

--- initialize.py
+++ initialize.py
@@ -7,6 +7,8 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Colonial_Heritage_Foundation.settings")
 django.setup()
 
+print('hi')
+
 # regular imports
 from account import models as amod
 from catalog import models as cmod
