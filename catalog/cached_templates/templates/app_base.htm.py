d05779f8edd837db6b11f51f5e14ae38da5453be
Jordan Widdison <jordan.widdison@gmail.com>
2016-03-08 18:54:36 -0700

    Added left and right column content to app base, so I didn't have to write it in each individual page.  Just make sure to still send the list of products and categories in the template vars to each template that needs them, because I couldn't send them to app_base.

--- catalog/cached_templates/templates/app_base.htm.py
+++ catalog/cached_templates/templates/app_base.htm.py
@@ -5,13 +5,13 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1457410652.419767
+_modified_time = 1457488374.295254
 _enable_loop = True
 _template_filename = '/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/catalog/templates/app_base.htm'
 _template_uri = 'app_base.htm'
 _source_encoding = 'utf-8'
 import os, os.path, re, json
-_exports = ['content_left', 'maintainence_container', 'top_content_area', 'maintainence_message', 'content', 'alert', 'content_right']
+_exports = ['content_right', 'content_left', 'maintainence_message', 'top_content_area', 'alert', 'maintainence_container']
 
 
 def _mako_get_namespace(context, name):
@@ -31,19 +31,18 @@ def render_body(context,**pageargs):
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         def content_right():
             return render_content_right(context._locals(__M_locals))
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
-        def maintainence_message():
-            return render_maintainence_message(context._locals(__M_locals))
-        def content():
-            return render_content(context._locals(__M_locals))
-        def alert():
-            return render_alert(context._locals(__M_locals))
-        def content_left():
-            return render_content_left(context._locals(__M_locals))
-        def maintainence_container():
-            return render_maintainence_container(context._locals(__M_locals))
         def top_content_area():
             return render_top_content_area(context._locals(__M_locals))
+        def maintainence_container():
+            return render_maintainence_container(context._locals(__M_locals))
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def content_left():
+            return render_content_left(context._locals(__M_locals))
+        def maintainence_message():
+            return render_maintainence_message(context._locals(__M_locals))
+        def alert():
+            return render_alert(context._locals(__M_locals))
+        categories = context.get('categories', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n\n# Leave these blocks blank\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'maintainence_container'):
@@ -60,27 +59,34 @@ def render_body(context,**pageargs):
             context['self'].alert(**pageargs)
         
 
-        __M_writer('\n# Leave left, center, and right blank\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content_left'):
-            context['self'].content_left(**pageargs)
-        
-
-        __M_writer('\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content_right'):
-            context['self'].content_right(**pageargs)
-        
-
-        __M_writer('\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
-            context['self'].content(**pageargs)
-        
-
         __M_writer('\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'top_content_area'):
             context['self'].top_content_area(**pageargs)
         
 
+        __M_writer('\n\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content_left'):
+            context['self'].content_left(**pageargs)
+        
+
+        __M_writer('\n\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content_right'):
+            context['self'].content_right(**pageargs)
+        
+
         __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content_right(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content_right():
+            return render_content_right(context)
+        __M_writer = context.writer()
+        __M_writer('\n  <h5>Recently Viewed</h5>\n  <hr />\n  <p>Probably have to store it in the session</p>\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -91,17 +97,23 @@ def render_content_left(context,**pageargs):
     try:
         def content_left():
             return render_content_left(context)
+        categories = context.get('categories', UNDEFINED)
         __M_writer = context.writer()
+        __M_writer('\n  <a class="btn btn-primary" href="/catalog/index/">View All Products</a>\n  <br />\n  <h5>Search</h5> # This needs to be a form! Use method="GET"\n  <hr />\n  <p>Text Box will go here.</p>\n  <br />\n  <h5>Category</h5>\n  <hr />\n  <a href="">View All</a>\n  <br />\n')
+        for category in categories:
+            __M_writer('    <a href="">')
+            __M_writer(str(category.name))
+            __M_writer('</a></li>\n    <br />\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
 
 
-def render_maintainence_container(context,**pageargs):
+def render_maintainence_message(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        def maintainence_container():
-            return render_maintainence_container(context)
+        def maintainence_message():
+            return render_maintainence_message(context)
         __M_writer = context.writer()
         return ''
     finally:
@@ -123,28 +135,6 @@ def render_top_content_area(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_maintainence_message(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def maintainence_message():
-            return render_maintainence_message(context)
-        __M_writer = context.writer()
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_content(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def content():
-            return render_content(context)
-        __M_writer = context.writer()
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 def render_alert(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
@@ -156,11 +146,11 @@ def render_alert(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_content_right(context,**pageargs):
+def render_maintainence_container(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        def content_right():
-            return render_content_right(context)
+        def maintainence_container():
+            return render_maintainence_container(context)
         __M_writer = context.writer()
         return ''
     finally:
@@ -169,6 +159,6 @@ def render_content_right(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"source_encoding": "utf-8", "line_map": {"148": 6, "100": 4, "73": 9, "170": 159, "119": 13, "78": 10, "111": 12, "48": 1, "83": 16, "137": 10, "53": 4, "118": 12, "89": 8, "120": 13, "68": 8, "58": 5, "159": 9, "28": 0, "126": 5, "63": 6}, "filename": "/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/catalog/templates/app_base.htm", "uri": "app_base.htm"}
+{"filename": "/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/catalog/templates/app_base.htm", "line_map": {"130": 8, "67": 12, "132": 9, "72": 29, "138": 6, "77": 35, "131": 9, "149": 4, "89": 31, "28": 0, "95": 14, "160": 149, "102": 14, "103": 25, "104": 26, "105": 26, "106": 26, "47": 1, "112": 5, "83": 31, "52": 4, "57": 5, "123": 8, "62": 6}, "uri": "app_base.htm", "source_encoding": "utf-8"}
 __M_END_METADATA
 """
--- catalog/cached_templates/templates/index.html.py
+++ catalog/cached_templates/templates/index.html.py
@@ -5,13 +5,13 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1457419948.799352
+_modified_time = 1457483543.123062
 _enable_loop = True
 _template_filename = '/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/catalog/templates/index.html'
 _template_uri = 'index.html'
 _source_encoding = 'utf-8'
 import os, os.path, re, json
-_exports = ['content_right', 'content_left', 'content']
+_exports = ['content']
 
 
 def _mako_get_namespace(context, name):
@@ -29,60 +29,16 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        categories = context.get('categories', UNDEFINED)
-        def content_right():
-            return render_content_right(context._locals(__M_locals))
-        def content_left():
-            return render_content_left(context._locals(__M_locals))
         def content():
             return render_content(context._locals(__M_locals))
         products = context.get('products', UNDEFINED)
         __M_writer = context.writer()
-        __M_writer('\n\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content_left'):
-            context['self'].content_left(**pageargs)
-        
-
-        __M_writer('\n\n')
+        __M_writer('\n\n\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
             context['self'].content(**pageargs)
         
 
-        __M_writer('\n\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content_right'):
-            context['self'].content_right(**pageargs)
-        
-
         __M_writer('\n')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_content_right(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def content_right():
-            return render_content_right(context)
-        __M_writer = context.writer()
-        __M_writer('\n  <h5>Recently Viewed</h5>\n  <hr />\n  <p>Probably have to store it in the session</p>\n')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_content_left(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        categories = context.get('categories', UNDEFINED)
-        def content_left():
-            return render_content_left(context)
-        __M_writer = context.writer()
-        __M_writer('\n  <a class="btn btn-primary">View All Products</a>\n  <br />\n  <h5>Search</h5> # This needs to be a form! Use method="GET"\n  <hr />\n  <p>Text Box will go here.</p>\n  <br />\n  <h5>Category</h5>\n  <hr />\n  <a href="">View All</a>\n  <br />\n')
-        for category in categories:
-            __M_writer('    <a href="">')
-            __M_writer(str(category.name))
-            __M_writer('</a></li>\n    <br />\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -115,6 +71,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"line_map": {"68": 30, "74": 3, "81": 3, "82": 14, "83": 15, "84": 15, "85": 15, "110": 26, "91": 20, "28": 0, "98": 20, "99": 21, "100": 22, "101": 22, "102": 22, "103": 23, "104": 23, "41": 1, "106": 24, "107": 25, "108": 25, "109": 26, "46": 18, "51": 28, "116": 110, "105": 24, "56": 34, "62": 30}, "uri": "index.html", "filename": "/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/catalog/templates/index.html", "source_encoding": "utf-8"}
+{"filename": "/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/catalog/templates/index.html", "line_map": {"64": 10, "65": 11, "66": 11, "36": 1, "72": 66, "41": 13, "47": 5, "60": 8, "54": 5, "55": 6, "56": 7, "57": 7, "58": 7, "59": 8, "28": 0, "61": 9, "62": 9, "63": 10}, "uri": "index.html", "source_encoding": "utf-8"}
 __M_END_METADATA
 """
