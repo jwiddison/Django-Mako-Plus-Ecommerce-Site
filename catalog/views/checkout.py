4d3a7102a0de0452aaeffa8425647ce38fe2f99b
Jordan Widdison <jordan.widdison@gmail.com>
2016-04-01 13:37:53 -0600

    Got google API working and pulling in the corrected information.  Those are presented to the user on the checkout.shipping page.

--- catalog/views/checkout.py
+++ catalog/views/checkout.py
@@ -27,17 +27,54 @@ def process_request(request):
     if request.method == 'POST':
         form = ShippingForm(request.POST)
         if form.is_valid():
-            # Create an instance of google maps
-            gmaps = googlemaps.Client(key=settings.GOOGLE_SERVER_KEY)
 
-            # Get full, concatenated Address
-            full_address = form.cleaned_data.get('shipping_address') + ', ' + form.cleaned_data.get('shipping_city') + ', ' + form.cleaned_data.get('shipping_state')
-            geocode_result = gmaps.geocode(full_address)
-            data = geocode_result.json()
+            # Get info from form.
+            name = form.cleaned_data.get('shipping_name')
+            # Get address information from the form and clear out spaces
+            address = form.cleaned_data.get('shipping_address')
+            address.replace(" ", "+")
+            city = form.cleaned_data.get('shipping_city')
+            city.replace(" ", "+")
+            state = form.cleaned_data.get('shipping_state')
+            state.replace(" ", "+")
+            zip_code = form.cleaned_data.get('shipping_zip_code')
 
-            print(data)
+            # Make full, concatenated Address
+            full_address = address + ',+' + city + ',+' + state
+            url="https://maps.googleapis.com/maps/api/geocode/json?address=%s" % full_address
 
-            return HttpResponseRedirect('/catalog/checkout.payment')
+            # Get results from google API and format them as JSON
+            resp = requests.get(url)
+            data = resp.json()
+
+            ###################################################################
+            #### Pull useable info out of the JSON response
+            # House Number
+            google_house_no = data['results'][0]['address_components'][0]['long_name']
+            # Street
+            google_street = data['results'][0]['address_components'][1]['short_name']
+            # Combine two together for street address
+            google_address = '%s %s' % (google_house_no, google_street)
+            # City
+            google_city = data['results'][0]['address_components'][2]['long_name']
+            # State
+            google_state = data['results'][0]['address_components'][4]['short_name']
+            # Zip Code
+            google_zip = data['results'][0]['address_components'][6]['long_name']
+
+            # Get lists out of session
+            google_response_list = request.session.get('google_address_response', [])
+            user_address_list = request.session.get('user_input_address', [])
+
+            # Override Lists from Session
+            google_response_list = [name, google_address, google_city, google_state, google_zip]
+            user_address_list = [name, address, city, state, zip_code]
+
+            # Set Lists back into session.
+            request.session['google_address_response'] = google_response_list
+            request.session['user_input_address'] = user_address_list
+
+            return HttpResponseRedirect('/catalog/checkout.shipping')
 
     template_vars['form'] = form
     return dmp_render_to_response(request, 'checkout.html', template_vars)
@@ -51,6 +88,12 @@ class ShippingForm(forms.Form):
     shipping_zip_code = forms.CharField(label='Shipping Zip Code', required=True, widget=forms.TextInput(attrs={'placeholder': 'Shipping Zip Code', 'class': 'form-control'}))
 
     # need to add clean methods to make sure the quantity is available.
+
+@view_function
+def shipping(request):
+    template_vars = initialize_template_vars(request)
+    return dmp_render_to_response(request, 'checkout.shipping.html', template_vars)
+
 
 @view_function
 @login_required(login_url='/catalog/login/')
