d2f623c727873e9fa1dd96461f06d7ee1822195f
Jordan Widdison <jordan.widdison@gmail.com>
2016-04-04 13:37:57 -0600

    Changed email form to a django form, and added styles.

--- homepage/cached_templates/templates/contact.html.py
+++ homepage/cached_templates/templates/contact.html.py
@@ -5,13 +5,13 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1459798347.183023
+_modified_time = 1459798599.989251
 _enable_loop = True
 _template_filename = '/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/homepage/templates/contact.html'
 _template_uri = 'contact.html'
 _source_encoding = 'utf-8'
 import os, os.path, re, json
-_exports = ['content_left', 'content_right', 'content', 'top_content_area']
+_exports = ['content', 'content_left', 'top_content_area', 'content_right']
 
 
 def _mako_get_namespace(context, name):
@@ -30,15 +30,15 @@ def render_body(context,**pageargs):
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         form = context.get('form', UNDEFINED)
-        sentmessage = context.get('sentmessage', UNDEFINED)
-        def content_left():
-            return render_content_left(context._locals(__M_locals))
         def content():
             return render_content(context._locals(__M_locals))
-        def content_right():
-            return render_content_right(context._locals(__M_locals))
+        def content_left():
+            return render_content_left(context._locals(__M_locals))
         def top_content_area():
             return render_top_content_area(context._locals(__M_locals))
+        def content_right():
+            return render_content_right(context._locals(__M_locals))
+        sentmessage = context.get('sentmessage', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'top_content_area'):
@@ -66,41 +66,30 @@ def render_body(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content():
+            return render_content(context)
+        form = context.get('form', UNDEFINED)
+        sentmessage = context.get('sentmessage', UNDEFINED)
+        __M_writer = context.writer()
+        __M_writer('\n  <div class="clearfix" id="link_to_form"></div>\n  <div class="row">\n    <div class="col-md-3"></div>\n    <div class="col-md-6">\n      <div class="center-block text-center">\n      <p>')
+        __M_writer(str(sentmessage))
+        __M_writer('</p>\n        <form class="form-horizontal text-center" method="POST">\n          <table>\n            ')
+        __M_writer(str(form.as_table()))
+        __M_writer('\n          </table>\n          <input type="submit" class="btn btn-primary" value="Send" id="send_button">\n          <input type="reset" class="btn btn-default" value="Clear" id="clear_button">\n        </form>\n      </div>\n    </div>\n    <div class="col-md-3"></div>\n  </div>\n\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
 def render_content_left(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         def content_left():
             return render_content_left(context)
         __M_writer = context.writer()
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_content_right(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def content_right():
-            return render_content_right(context)
-        __M_writer = context.writer()
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_content(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        form = context.get('form', UNDEFINED)
-        sentmessage = context.get('sentmessage', UNDEFINED)
-        def content():
-            return render_content(context)
-        __M_writer = context.writer()
-        __M_writer('\n  <div class="clearfix" id="link_to_form"></div>\n  <p>')
-        __M_writer(str(sentmessage))
-        __M_writer('</p>\n  <div class="center-block text-center">\n    <form class="form-horizontal text-center" method="POST">\n      <table>\n        ')
-        __M_writer(str(form.as_table()))
-        __M_writer('\n      </table>\n      <input type="submit" class="btn btn-primary" value="Send" id="send_button">\n      <input type="reset" class="btn btn-default" value="Clear" id="clear_button">\n    </form>\n  </div>\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -118,8 +107,19 @@ def render_top_content_area(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
+def render_content_right(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content_right():
+            return render_content_right(context)
+        __M_writer = context.writer()
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
 """
 __M_BEGIN_METADATA
-{"filename": "/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/homepage/templates/contact.html", "source_encoding": "utf-8", "line_map": {"80": 66, "99": 13, "100": 15, "69": 65, "102": 19, "103": 19, "43": 1, "101": 15, "109": 3, "48": 11, "115": 3, "53": 63, "121": 115, "58": 65, "91": 13, "28": 0, "63": 66}, "uri": "contact.html"}
+{"line_map": {"80": 22, "98": 3, "69": 13, "104": 3, "43": 1, "77": 13, "78": 19, "79": 19, "48": 11, "81": 22, "53": 70, "87": 72, "110": 73, "121": 110, "58": 72, "28": 0, "63": 73}, "source_encoding": "utf-8", "filename": "/Users/Jordan/Documents/BYU/0 - Senior Year/0 - Winter 2016/0 - 413/Colonial_Heritage_Foundation/homepage/templates/contact.html", "uri": "contact.html"}
 __M_END_METADATA
 """
